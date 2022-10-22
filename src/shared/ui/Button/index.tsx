67d81039763c27d8417344284b20b839109bbca9
Some1 <111959143+s1i1@users.noreply.github.com>
2022-10-22 15:23:35 +0300

    Сделал Button

---
+++ src/shared/ui/Button/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import cn from 'classnames';
+import arrowPath from '../../../assets/img/arrow.svg';
+import styles from './Button.module.scss';
+
+type ButtonProps = {
+  title: string;
+  isArrow?: boolean;
+  isLarge?: boolean;
+};
+
+export const Button: React.FC<ButtonProps> = ({ title, isArrow, isLarge }) => {
+  return (
+    <div className={styles.button}>
+      <div className={cn(styles.container, isLarge && styles.largeButton)}>
+        <div className={styles.title}>{title}</div>
+        {isArrow && <img className={styles.image} src={arrowPath} alt="arrow" />}
+      </div>
+    </div>
+  );
+};
