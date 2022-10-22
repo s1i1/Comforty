bf89c77ab74438dce68a0889b7f3119df7d7b8d1
Some1 <111959143+s1i1@users.noreply.github.com>
2022-10-22 13:53:46 +0300

    Сделал ArrowButton

---
+++ src/shared/ui/ArrowButton/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import cn from 'classnames';
+import styles from './ArrowButton.module.scss';
+
+type ArrowProps = {
+  rotate?: boolean;
+};
+
+export const ArrowButton: React.FC<ArrowProps> = ({ rotate }) => {
+  return (
+    <div className={styles.arrow}>
+      <svg
+        className={cn(rotate && styles.rotate__arrow)}
+        width="44"
+        height="44"
+        viewBox="0 0 44 44"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg">
+        <rect width="44" height="44" rx="22" fill="#F0F2F3" />
+        <path
+          d="M25.5 17.5L30 22M30 22L25.5 26.5M30 22H14"
+          stroke="#272343"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+    </div>
+  );
+};
