edf30b6b2bea856a75078bcd07c2e4fb0995c017
Some1 <111959143+s1i1@users.noreply.github.com>
2022-10-23 14:51:35 +0300

    Сделал Logo

---
+++ src/shared/ui/Logo/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logoIcon from './assets/logo-icon.svg';
+import styles from './Logo.module.scss';
+
+export const Logo = () => {
+  return (
+    <div>
+      <div className={styles.container}>
+        <img className={styles.image} src={logoIcon} alt="logoIcon" />
+        <div className={styles.title}>Comforty</div>
+      </div>
+    </div>
+  );
+};
