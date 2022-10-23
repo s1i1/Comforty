ea41050dec299fdfc785fcd7149312cf07ed9d95
Some1 <111959143+s1i1@users.noreply.github.com>
2022-10-23 15:56:43 +0300

    Сделал NewIcon

---
+++ src/shared/ui/NewIcon/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './NewIcon.module.scss';
+
+export const NewIcon = () => {
+  return (
+    <div>
+      <div className={styles.container}>
+        <div className={styles.text}>New</div>
+      </div>
+    </div>
+  );
+};
