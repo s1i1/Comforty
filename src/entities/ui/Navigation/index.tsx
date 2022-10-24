409fd489237e56feb336dad9f8109157a9f0f181
Some1 <111959143+s1i1@users.noreply.github.com>
2022-10-24 18:02:14 +0300

    В Navigation добавил .active

--- src/entities/ui/Navigation/index.tsx
+++ src/entities/ui/Navigation/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import cn from 'classnames';
 import styles from './Navigation.module.scss';
 
 type NavigationProps = {
   title: string;
+  isActive?: boolean;
 };
 
-export const Navigation: React.FC<NavigationProps> = ({ title }) => {
-  return <li className={styles.title}>{title}</li>;
+export const Navigation: React.FC<NavigationProps> = ({ title, isActive }) => {
+  return <li className={cn(styles.title, isActive && styles.active)}>{title}</li>;
 };
