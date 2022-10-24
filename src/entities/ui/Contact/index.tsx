1ee4976361aec6b8de95d6b014ae3f9d2734bba2
Some1 <111959143+s1i1@users.noreply.github.com>
2022-10-24 15:37:33 +0300

    В Contact пофиксил стили

--- src/entities/ui/Contact/index.tsx
+++ src/entities/ui/Contact/index.tsx
@@ -3,7 +3,7 @@ import styles from './Contact.module.scss';
 
 export const Contact = () => {
   return (
-    <div>
+    <div className={styles.contact}>
       <div>
         <p className={styles.title}>Contact:</p>
         <p className={styles.phone}>(808) 555-0111</p>
