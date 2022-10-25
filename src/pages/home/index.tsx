9a0ef6ef50b49a5e1cd093d03747c1b9cd16d367
Some1 <111959143+s1i1@users.noreply.github.com>
2022-10-25 16:34:42 +0300

    Сделал MainSlider

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { MainSlider } from 'widgets/main-slider';
 import { Header } from '../../widgets/header';
 import styles from './home.module.scss';
 
 export const HomePage = () => {
   return (
     <div className={styles.homepage}>
-      <Header />
+      <div className={styles.header}>
+        <Header />
+      </div>
+      <MainSlider />
     </div>
   );
 };
