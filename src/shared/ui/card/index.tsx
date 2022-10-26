fb95d3061a292891379545aee31f85cb3bd2bd37
Some1 <111959143+s1i1@users.noreply.github.com>
2022-10-26 18:10:57 +0300

    Сделал card

---
+++ src/shared/ui/card/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import cn from 'classnames';
+import styles from './styles.module.scss';
+import { ProductTag } from 'shared/ui';
+import { AddCartButton, FavoriteButton } from '../buttons';
+
+type CardProps = {
+  imagePath: string;
+};
+
+const Card: React.FC<CardProps> = ({ imagePath }) => {
+  return (
+    <div className={cn(styles.container)}>
+      <div className={styles.tag}>
+        <ProductTag />
+      </div>
+      <div className={styles.favorite}>
+        <FavoriteButton />
+      </div>
+
+      <img className={styles.background} src={imagePath} alt="product-background" />
+
+      <div className={styles.bottom}>
+        <div className={styles.product__info}>
+          <p className={styles.text}>Library Stool Chair</p>
+          <span className={styles.current__price}>$20</span>
+          <span className={styles.prev__price}>$39</span>
+        </div>
+
+        <div className={styles.cart}>
+          <AddCartButton isActive />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- src/shared/ui/index.tsx
+++ src/shared/ui/index.tsx
@@ -1,3 +1,4 @@
 export { default as Logo } from './logo';
 export { default as ProductTag } from './product-tag';
 export { default as ProductDiscount } from './product-discount';
+export { default as Card } from './card';
