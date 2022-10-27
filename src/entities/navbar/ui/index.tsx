d11071b9fd38c736d9e25cb1199d59364dcda90c
Some1 <111959143+s1i1@users.noreply.github.com>
2022-10-27 11:46:14 +0300

    Переименовал Navigation в Navbar

--- src/entities/navbar/ui/index.tsx
+++ src/entities/navbar/ui/index.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import cn from 'classnames';
 import styles from './styles.module.scss';
 
-type NavigationProps = {
+type NavbarProps = {
   title: string;
   isActive?: boolean;
 };
 
-const Navigation: React.FC<NavigationProps> = ({ title, isActive }) => {
+const Navbar: React.FC<NavbarProps> = ({ title, isActive }) => {
   return <li className={cn(styles.title, isActive && styles.active)}>{title}</li>;
 };
 
-export default Navigation;
+export default Navbar;
