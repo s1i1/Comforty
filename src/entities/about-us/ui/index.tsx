f43edb6d5d8d7a0ad5cb3c4cb240b8a5e72c285f
Some1 <111959143+s1i1@users.noreply.github.com>
2022-10-27 18:07:15 +0300

    Сделал и адаптировал AboutUs

---
+++ src/entities/about-us/ui/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import cn from 'classnames';
+import imagePath from './assets/test-image.svg';
+import CardAbout from 'shared/ui/card-about';
+import { Heading } from 'entities/heading';
+
+import styles from './styles.module.scss';
+
+const AboutUs = () => {
+  const testData = [
+    {
+      imagePath: imagePath,
+      name: 'Kristin Watson',
+      profession: 'Fashion Designer',
+      text: '“Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet mi nec massa tincidunt blandit et eu sem. Maecenas laoreet ultrices diam dignissim posuere. Aenean ultrices dui at ipsum sagittis, pharetra lacinia dui faucibus. In ac bibendum ex. Aenean dolor massa, euismod sit amet suscipit et“',
+    },
+    {
+      imagePath: imagePath,
+      name: 'Kristin Watson',
+      profession: 'Fashion Designer',
+      text: '“Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet mi nec massa tincidunt blandit et eu sem. Maecenas laoreet ultrices diam dignissim posuere. Aenean ultrices dui at ipsum sagittis, pharetra lacinia dui faucibus. In ac bibendum ex. Aenean dolor massa, euismod sit amet suscipit et“',
+    },
+  ];
+
+  return (
+    <div className={styles.about}>
+      <div className={cn('_container', styles.container)}>
+        <Heading title="What client says about us" />
+
+        <ul className={styles.list}>
+          {testData.map((obj, index) => (
+            <li key={index}>
+              <CardAbout
+                text={obj.text}
+                imageUrl={obj.imagePath}
+                name={obj.name}
+                profession={obj.profession}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default AboutUs;
