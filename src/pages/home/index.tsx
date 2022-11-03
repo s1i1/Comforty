21eb6a70320bb9fae48283d4bb6e79b63aa279cf
Some1 <111959143+s1i1@users.noreply.github.com>
2022-11-03 10:53:57 +0300

    Перенес ProductCard в features

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { MainSlider } from 'widgets/main-slider';
 import { FeaturesBlock } from 'widgets/features-block';
 import { CompaniesLogo } from 'widgets/companies-logo';
-import { ProductCard } from 'entities/product-card';
+import { ProductCard } from 'features/product-card';
 import { TopCategories } from 'entities/top-categories';
 import { OurProducts } from 'entities/our-products';
 import { AboutUs } from 'widgets/about-us';
