6c954f512913dab0b1512cc8bda6e7316be2638c
Some1 <111959143+s1i1@users.noreply.github.com>
2022-11-09 15:23:50 +0300

    Сделал в search-product двухстороннее связывание инпута

--- src/features/search-product/ui/index.tsx
+++ src/features/search-product/ui/index.tsx
@@ -4,19 +4,26 @@ import clearIcon from './assets/clear-icon.svg';
 import searchIcon from './assets/search-icon.svg';
 import styles from './styles.module.scss';
 
-type SearchProductProps = {
-  isValue?: boolean;
-};
+const SearchProduct: React.FC = () => {
+  const [searchValue, setSearchValue] = React.useState('');
 
-const SearchProduct: React.FC<SearchProductProps> = ({ isValue }) => {
-  const iconPath = isValue ? clearIcon : searchIcon;
+  const iconPath = searchValue ? clearIcon : searchIcon;
+
+  const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value);
+  };
 
   return (
     <div className={styles.container}>
       <div className={styles.search}>
-        <input type="text" placeholder="Search here..." />
+        <input
+          type="text"
+          placeholder="Search here..."
+          value={searchValue}
+          onChange={(e) => onChangeSearch(e)}
+        />
         <img
-          className={cn(styles.image, isValue && styles.clear)}
+          className={cn(styles.image, searchValue && styles.clear)}
           src={iconPath}
           alt="searchIcon"
         />
