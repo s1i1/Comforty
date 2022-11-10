253b1f966f0c9b297fc826019c64252a7562d5f1
Some1 <111959143+s1i1@users.noreply.github.com>
2022-11-10 18:15:04 +0300

    Сделал shop-page

--- src/pages/shop/index.tsx
+++ src/pages/shop/index.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import cn from 'classnames';
+import styles from './styles.module.scss';
+import { OurProducts } from 'entities/our-products';
 
 export const ShopPage = () => {
-  return <div>index</div>;
+  return (
+    <div className={cn('_container', styles.container)}>
+      <OurProducts />
+    </div>
+  );
 };
