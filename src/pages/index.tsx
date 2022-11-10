433c4dfecd4ca642a1ec439e3f3f331a8104ecb7
Some1 <111959143+s1i1@users.noreply.github.com>
2022-11-10 14:25:09 +0300

    Создал shop-page, добавил в роутинг

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import { HomePage } from './home';
+import { ShopPage } from './shop';
 
 export const Routing = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="shop" element={<ShopPage />} />
     </Routes>
   );
 };
---
+++ src/pages/shop/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const ShopPage = () => {
+  return <div>index</div>;
+};
