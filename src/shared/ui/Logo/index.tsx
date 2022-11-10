1f9f014372180522431cc3d75ec0af324024986b
Some1 <111959143+s1i1@users.noreply.github.com>
2022-11-10 15:01:32 +0300

    В logo добавил link to

--- src/shared/ui/Logo/index.tsx
+++ src/shared/ui/Logo/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { baseRoutes } from 'shared/lib';
 import logoIcon from './assets/logo-icon.svg';
 import styles from './styles.module.scss';
 
 const Logo = () => {
   return (
     <div className={styles.logo}>
-      <div className={styles.container}>
+      <Link to={baseRoutes.HOME} className={styles.container}>
         <img className={styles.image} src={logoIcon} alt="logoIcon" />
         <div className={styles.title}>Comforty</div>
-      </div>
+      </Link>
     </div>
   );
 };
