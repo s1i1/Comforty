f1f57ca6580eee5b907fb4b27ceac2a19423becf
Some1 <111959143+s1i1@users.noreply.github.com>
2022-11-10 17:09:43 +0300

    В logo добавил setActivePage(0)

--- src/shared/ui/Logo/index.tsx
+++ src/shared/ui/Logo/index.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { baseRoutes } from 'shared/lib';
+import { baseRoutes, useAppDispatch } from 'shared/lib';
+import { navbarModel } from 'features/navbar';
 import logoIcon from './assets/logo-icon.svg';
 import styles from './styles.module.scss';
 
 const Logo = () => {
+  const dispatch = useAppDispatch();
+
   return (
     <div className={styles.logo}>
-      <Link to={baseRoutes.HOME} className={styles.container}>
+      <Link
+        to={baseRoutes.HOME}
+        className={styles.container}
+        onClick={() => dispatch(navbarModel.setActivePage(0))}>
         <img className={styles.image} src={logoIcon} alt="logoIcon" />
         <div className={styles.title}>Comforty</div>
       </Link>
