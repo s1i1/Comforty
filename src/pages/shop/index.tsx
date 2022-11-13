bc4655061dd79651b1a00e9188f4ca23b3e2b6e9
Some1 <111959143+s1i1@users.noreply.github.com>
2022-11-13 19:18:29 +0300

    Убрал из shop-page _container

--- src/pages/shop/index.tsx
+++ src/pages/shop/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import cn from 'classnames';
 import styles from './styles.module.scss';
 import { OurProducts } from 'entities/our-products';
 
 export const ShopPage = () => {
   return (
-    <div className={cn('_container', styles.container)}>
+    <div className={styles.container}>
       <OurProducts />
     </div>
   );
