cf08ade988c0dde43a982d3db6564c6275ea4c2d
Some1 <111959143+s1i1@users.noreply.github.com>
2022-11-14 18:28:32 +0300

    Сделал в top-categories при клике роутинг на SHOP с выбранной категорией

--- src/entities/top-categories/ui/index.tsx
+++ src/entities/top-categories/ui/index.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import cn from 'classnames';
+import { Link } from 'react-router-dom';
+import { useAppDispatch, baseRoutes } from 'shared/lib';
 import { CategoryCard } from 'shared/ui';
 import { Heading } from 'entities/heading';
 import { useGetTopCategoriesQuery } from 'shared/api';
+import { categoriesModel } from 'features/categories';
 import styles from './styles.module.scss';
 
 const TopCategories = () => {
+  const dispatch = useAppDispatch();
+
   const [pageNum, setPageNum] = React.useState(1);
 
   const { data: products } = useGetTopCategoriesQuery({ limit: 3, pageNumber: pageNum });
+
+  const handlerClickCategory = (categoryName: string) => {
+    categoriesModel.categoryNames.forEach((obj) => {
+      if (obj.title === categoryName) {
+        dispatch(categoriesModel.setCurrentCategory(obj));
+      }
+    });
+  };
 
   return (
     <div className={cn('_container', styles.container)}>
@@ -16,8 +29,10 @@ const TopCategories = () => {
 
       <ul className={styles.list}>
         {products?.map((product, index) => (
-          <li key={index}>
-            <CategoryCard {...product} />
+          <li key={index} onClick={() => handlerClickCategory(product.category)}>
+            <Link to={baseRoutes.SHOP}>
+              <CategoryCard {...product} />
+            </Link>
           </li>
         ))}
       </ul>
