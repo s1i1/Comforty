2ad0d555d8bd3b6b9736f8117f0522804035db3f
Some1 <111959143+s1i1@users.noreply.github.com>
2022-11-16 14:26:18 +0300

    В language добавил rotate

--- src/features/language/language-list/ui/index.tsx
+++ src/features/language/language-list/ui/index.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
+import cn from 'classnames';
 import arrow from './assets/arrow.svg';
 import styles from './styles.module.scss';
 
 type LanguageProps = {
   whatLanguage: string;
+  rotate?: boolean;
 };
 
-const Language: React.FC<LanguageProps> = ({ whatLanguage }) => {
+const Language: React.FC<LanguageProps> = ({ whatLanguage, rotate }) => {
   return (
     <div className={styles.container}>
       <p>{whatLanguage}</p>
-      <img src={arrow} alt="arrow" />
+      <img src={arrow} alt="arrow" className={cn(rotate && styles.rotate)} />
     </div>
   );
 };
