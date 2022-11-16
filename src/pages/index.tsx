d080b496225b639d194b315e98e376551925571c
Some1 <111959143+s1i1@users.noreply.github.com>
2022-11-16 13:14:18 +0300

    Сделал sctollTo(0,0) при изменении location.pathname

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,15 @@
-import { Routes, Route } from 'react-router-dom';
+import React from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import { HomePage } from './home';
 import { ShopPage } from './shop';
 
 export const Routing = () => {
+  const { pathname } = useLocation();
+
+  React.useEffect(() => {
+    document.querySelector('#root')?.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
