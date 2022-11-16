cff1db22c05a8081ae4a902f387c2fe7a714ef65
Some1 <111959143+s1i1@users.noreply.github.com>
2022-11-16 13:32:50 +0300

    В shared создал baseLinks

---
+++ src/shared/lib/base-links/index.ts
@@ -0,0 +1,7 @@
+export const baseLinks = {
+  FACEBOOK: 'https://www.facebook.com/',
+  TWITTER: 'https://twitter.com/',
+  INSTAGRAM: 'https://www.instagram.com/',
+  PINTEREST: 'https://www.pinterest.com/',
+  YOUTUBE: 'https://www.youtube.com/',
+};
