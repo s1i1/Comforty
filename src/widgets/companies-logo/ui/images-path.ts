b67308ff5371f725213f29adf5e11fb408dcc300
Some1 <111959143+s1i1@users.noreply.github.com>
2022-11-17 17:48:17 +0300

    В companies-logo assets заменил картинки jpg на svg

--- src/widgets/companies-logo/ui/images-path.ts
+++ src/widgets/companies-logo/ui/images-path.ts
@@ -1,9 +1,9 @@
-import zapier from './assets/zapier-logo.jpg';
-import pipedrive from './assets/pipedrive-logo.jpg';
-import cibBank from './assets/cib-bank-logo.jpg';
-import just from './assets/just-logo.jpg';
-import burntToast from './assets/burnt-toast-logo.jpg';
-import pandaDoc from './assets/panda-doc-logo.jpg';
-import moz from './assets/moz-logo.jpg';
+import zapier from './assets/zapier-logo.svg';
+import pipedrive from './assets/pipedrive-logo.svg';
+import cibBank from './assets/cib-bank-logo.svg';
+import just from './assets/just-logo.svg';
+import burntToast from './assets/burnt-toast-logo.svg';
+import pandaDoc from './assets/panda-doc-logo.svg';
+import moz from './assets/moz-logo.svg';
 
 export const imagesPath = [zapier, pipedrive, cibBank, just, burntToast, pandaDoc, moz];
