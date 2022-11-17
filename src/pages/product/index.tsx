cc44e194aa01864965698c6b0770710d41fd5310
Some1 <111959143+s1i1@users.noreply.github.com>
2022-11-17 15:13:27 +0300

    Прикрутил логику для ProductPage

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,19 +1,37 @@
 import React from 'react';
 import cn from 'classnames';
-import { CategoryCard } from 'shared/ui';
+import { Link } from 'react-router-dom';
 import { useGetTopCategoriesQuery } from 'shared/api';
+import { useAppDispatch, baseRoutes } from 'shared/lib';
+import { CategoryCard } from 'shared/ui';
+import { categoriesModel } from 'features/categories';
 import styles from './styles.module.scss';
 
 export const ProductPage = () => {
-  const { data: product } = useGetTopCategoriesQuery({ limit: 6, pageNumber: 1 });
+  const dispatch = useAppDispatch();
+
+  const { data: products } = useGetTopCategoriesQuery({ limit: 6, pageNumber: 1 });
+
+  const handlerClickCategory = (categoryName: string) => {
+    categoriesModel.categoryNames.forEach((obj) => {
+      if (obj.title === categoryName) {
+        dispatch(categoriesModel.setCurrentCategory(obj));
+      }
+    });
+  };
 
   return (
     <div className={cn('_container', styles.container)}>
       <h2 className={styles.header}>Our Categories</h2>
       <ul className={styles.list}>
-        {product?.map((obj) => (
-          <li key={obj.id} className={styles.list__item}>
-            <CategoryCard {...obj} />
+        {products?.map((product) => (
+          <li
+            key={product.id}
+            className={styles.list__item}
+            onClick={() => handlerClickCategory(product.category)}>
+            <Link to={baseRoutes.SHOP}>
+              <CategoryCard {...product} />
+            </Link>
           </li>
         ))}
       </ul>
