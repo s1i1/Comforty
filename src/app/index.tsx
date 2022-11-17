495a16916de83da032bd385779fcea3c47c35de9
Some1 <111959143+s1i1@users.noreply.github.com>
2022-11-17 15:22:23 +0300

    Из Routing перенес логику скрола страницы при смене роута в App

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import { useLocation } from 'react-router-dom';
 import { Routing } from 'pages';
 import { Header } from 'widgets/header';
 import { Footer } from 'widgets/footer';
@@ -5,6 +7,12 @@ import { Footer } from 'widgets/footer';
 import './styles/index.scss';
 
 const App = () => {
+  const { pathname } = useLocation();
+
+  React.useEffect(() => {
+    document.querySelector('#root')?.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <div className="App">
       <Header />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,10 @@
 import React from 'react';
-import { Routes, Route, useLocation } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { HomePage } from './home';
 import { ShopPage } from './shop';
 import { ProductPage } from './product';
 
 export const Routing = () => {
-  const { pathname } = useLocation();
-
-  React.useEffect(() => {
-    document.querySelector('#root')?.scrollTo(0, 0);
-  }, [pathname]);
-
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
