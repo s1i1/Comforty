e4a71f7ca4cca97d805f931ceb0f1ab35feb2291
Some1 <111959143+s1i1@users.noreply.github.com>
2022-11-17 17:27:00 +0300

    Подправил структуру hooks

--- src/shared/lib/hooks/redux.ts
+++ src/shared/lib/hooks/redux.ts
@@ -1,4 +1,5 @@
-export * from './redux';
+import { useDispatch, useSelector, TypedUseSelectorHook } from 'react-redux';
+import { AppDispatch, RootState } from 'app/store/types';
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
 
