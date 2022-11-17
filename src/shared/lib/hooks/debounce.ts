ad3389844abc737cbd8eec9a5f5c13853fb8303f
Some1 <111959143+s1i1@users.noreply.github.com>
2022-11-17 17:27:33 +0300

    Создал хук debounce

---
+++ src/shared/lib/hooks/debounce.ts
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const useDebounce = (value: string, delay: number = 300): string => {
+  const [debounced, setDebounced] = React.useState(value);
+
+  React.useEffect(() => {
+    const handler = setTimeout(() => setDebounced(value), delay);
+
+    return () => clearTimeout(handler);
+  }, [value, delay]);
+
+  return debounced;
+};
--- src/shared/lib/hooks/index.ts
+++ src/shared/lib/hooks/index.ts
@@ -1,5 +1,2 @@
 export * from './redux';
-
-export const useAppDispatch: () => AppDispatch = useDispatch;
-
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export * from './debounce';
