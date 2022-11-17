5aa81d1eaeb433126405d5fe7f5f81cc3ecd1042
Some1 <111959143+s1i1@users.noreply.github.com>
2022-11-17 12:44:18 +0300

    В Logo удалил сброс категории

--- src/shared/ui/Logo/index.tsx
+++ src/shared/ui/Logo/index.tsx
@@ -1,22 +1,13 @@
 import React from 'react';
-import { useAppDispatch } from 'shared/lib';
 import { Link } from 'react-router-dom';
 import { baseRoutes } from 'shared/lib';
-import { categoriesModel } from 'features/categories';
 import logoIcon from './assets/logo-icon.svg';
 import styles from './styles.module.scss';
 
 const Logo = () => {
-  const dispatch = useAppDispatch();
-  const { categoryNames, setCurrentCategory } = categoriesModel;
-
-  const handlerClickLogo = () => {
-    dispatch(setCurrentCategory(categoryNames[0]));
-  };
-
   return (
     <div className={styles.logo}>
-      <Link to={baseRoutes.HOME} className={styles.container} onClick={handlerClickLogo}>
+      <Link to={baseRoutes.HOME} className={styles.container}>
         <img className={styles.image} src={logoIcon} alt="logoIcon" />
         <div className={styles.title}>Comforty</div>
       </Link>
