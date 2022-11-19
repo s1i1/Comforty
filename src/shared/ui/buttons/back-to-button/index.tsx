908e8aa3592c2e11ef63d7c416cfc6f2035c1942
Some1 <111959143+s1i1@users.noreply.github.com>
2022-11-19 17:14:06 +0300

    Сделал back-to-button

---
+++ src/shared/ui/buttons/back-to-button/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import ArrowButton from '../arrow-button';
+import styles from './styles.module.scss';
+
+type BackToButtonProps = {
+  title: string;
+};
+
+const BackToButton: React.FC<BackToButtonProps> = ({ title }) => {
+  return (
+    <div className={styles.container}>
+      <svg
+        className={styles.image}
+        width="30"
+        height="30"
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg">
+        <path
+          d="M15.5 7.5L20 12M20 12L15.5 16.5M20 12H4"
+          stroke="white"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+
+      <p className={styles.text}>{title}</p>
+    </div>
+  );
+};
+
+export default BackToButton;
--- src/shared/ui/buttons/index.ts
+++ src/shared/ui/buttons/index.ts
@@ -5,3 +5,4 @@ export { default as FavoriteButton } from './favorite-button';
 export { default as ProfileButton } from './profile-button';
 export { default as CategoriesButton } from './categories-button';
 export { default as ToTopButton } from './to-top-button';
+export { default as BackToButton } from './back-to-button';
