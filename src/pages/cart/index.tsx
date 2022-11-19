4b81b2b8f8bd6d476fad6683d74d7629a768467b
Some1 <111959143+s1i1@users.noreply.github.com>
2022-11-19 18:25:38 +0300

    Сделал CartPage

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,6 +1,20 @@
 import React from 'react';
-import styles from './styles.module.scss';
+import { CardBlock } from 'widgets/card-block';
 
 export const CartPage = () => {
-  return <div>CartPage</div>;
+  const testArr = [
+    { image: 'https://i.imgur.com/X2sAq9u.jpg', price: 100, title: "Barny 78'' Upholstered" },
+    { image: 'https://i.imgur.com/X2sAq9u.jpg', price: 100, title: "Barny 78'' Upholstered" },
+    { image: 'https://i.imgur.com/X2sAq9u.jpg', price: 100, title: "Barny 78'' Upholstered" },
+    { image: 'https://i.imgur.com/X2sAq9u.jpg', price: 100, title: "Barny 78'' Upholstered" },
+    { image: 'https://i.imgur.com/X2sAq9u.jpg', price: 100, title: "Barny 78'' Upholstered" },
+    { image: 'https://i.imgur.com/X2sAq9u.jpg', price: 100, title: "Barny 78'' Upholstered" },
+    { image: 'https://i.imgur.com/X2sAq9u.jpg', price: 100, title: "Barny 78'' Upholstered" },
+    { image: 'https://i.imgur.com/X2sAq9u.jpg', price: 100, title: "Barny 78'' Upholstered" },
+  ];
+  return (
+    <div>
+      <CardBlock header="Your Shopping Cart" products={testArr} />
+    </div>
+  );
 };
