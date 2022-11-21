c14d715410ca9cf29c5106434057774ec7a6d696
Some1 <111959143+s1i1@users.noreply.github.com>
2022-11-21 15:50:12 +0300

    Сделал cart-empty

--- src/entities/cart/cart-empty/ui/index.tsx
+++ src/entities/cart/cart-empty/ui/index.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import cn from 'classnames';
+import { Link } from 'react-router-dom';
+import { baseRoutes } from 'shared/lib';
+import { BackToButton } from 'shared/ui/buttons';
 import styles from './styles.module.scss';
 
 const CartEmpty = () => {
-  return <div>CartEmpty</div>;
+  return (
+    <div className={cn('_container', styles.container)}>
+      <p className={styles.text}>Cart is empty</p>
+      <Link to={baseRoutes.SHOP} className={styles.back__to_button}>
+        <BackToButton title="continue shopping" />
+      </Link>
+    </div>
+  );
 };
 
 export default CartEmpty;
