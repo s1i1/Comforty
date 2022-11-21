feca2c4083949fb33214f23c946bd82298480693
Some1 <111959143+s1i1@users.noreply.github.com>
2022-11-21 19:53:55 +0300

    В cart-item добавил .disabled для .minus__button

--- src/shared/ui/cart-item/index.tsx
+++ src/shared/ui/cart-item/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import cn from 'classnames';
 import { useAppDispatch } from 'shared/lib';
 import { cartPageModel } from 'pages/cart';
 import styles from './styles.module.scss';
@@ -40,17 +41,19 @@ const CartItem: React.FC<CartItemProps> = ({ id, image, price, title, count }) =
       </div>
 
       <div className={styles.count}>
-        <div className={styles.minus__button} onClick={onClickDecrementCount}>
+        <div
+          className={cn(count && count > 1 ? styles.minus__button : styles.disabled)}
+          onClick={onClickDecrementCount}>
           <svg
             width="32"
             height="32"
             viewBox="0 0 32 32"
             fill="none"
             xmlns="http://www.w3.org/2000/svg">
-            <circle cx="16" cy="16" r="15" fill="white" stroke="#FE5F1E" strokeWidth="2" />
+            <circle cx="16" cy="16" r="15" fill="white" stroke="#007580" strokeWidth="2" />
             <path
               d="M15.0402 15.04H19.8402C20.3704 15.04 20.8002 15.4698 20.8002 16C20.8002 16.5302 20.3704 16.96 19.8402 16.96H15.0402H12.1602C11.63 16.96 11.2002 16.5302 11.2002 16C11.2002 15.4698 11.63 15.04 12.1602 15.04H15.0402Z"
-              fill="#FE5F1E"
+              fill="#007580"
             />
           </svg>
         </div>
