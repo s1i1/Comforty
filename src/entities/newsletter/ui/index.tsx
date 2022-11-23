6497db18704a8a99659a0bb58f70b6612446fbfd
Some1 <111959143+s1i1@users.noreply.github.com>
2022-11-23 12:34:54 +0300

    Прикрутил логику для newsletter

--- src/entities/newsletter/ui/index.tsx
+++ src/entities/newsletter/ui/index.tsx
@@ -3,14 +3,25 @@ import { Button } from 'shared/ui/buttons';
 import styles from './styles.module.scss';
 
 const Newsletter = () => {
+  const [value, setValue] = React.useState('');
+
+  const handlerClick = () => {
+    setValue('');
+  };
+
   return (
     <div className={styles.container}>
       <h4 className={styles.header}>Newsletter</h4>
-      <label className={styles.mail}>
-        <input type="text" placeholder="Your email" />
+      <label className={styles.mail} onClick={handlerClick}>
+        <input
+          type="text"
+          placeholder="Your email"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
         <Button title="Subscribe" />
       </label>
-      <p className={styles.text}>Sign up for our newsletter</p>
+      {/* <p className={styles.text}>Sign up for our newsletter</p> */}
     </div>
   );
 };
