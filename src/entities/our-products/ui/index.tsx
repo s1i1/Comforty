611aa052559ff1f8deeea20a517c74ccf349e3a4
Some1 <111959143+s1i1@users.noreply.github.com>
2022-11-23 14:22:37 +0300

    В our-products добавил скелетон

--- src/entities/our-products/ui/index.tsx
+++ src/entities/our-products/ui/index.tsx
@@ -10,6 +10,8 @@ import { ProductItems } from 'shared/api/mock.api/models';
 import { categoriesModel } from 'features/categories';
 import { searchModel } from 'features/search-product';
 import { SortNamesItems } from '../model';
+import ContentLoader from 'react-content-loader';
+
 import styles from './styles.module.scss';
 
 const OurProducts = () => {
@@ -40,7 +42,11 @@ const OurProducts = () => {
 
   const totalPages = [1, 2, 3, 4, 5, 6];
 
-  const { data: products, isSuccess } = useGetItemsQuery({
+  const {
+    data: products,
+    isSuccess,
+    isFetching,
+  } = useGetItemsQuery({
     pageNumber: pageNumber,
     tag: linkTag || currentCategory.category,
     limit: 8,
@@ -147,7 +153,22 @@ const OurProducts = () => {
       <ul className={styles.products__list}>
         {productItems?.map((obj: ProductItems) => (
           <li key={obj.id}>
-            <Card {...obj} />
+            {isFetching ? (
+              <ContentLoader
+                speed={1}
+                width={312}
+                height={422}
+                viewBox="0 0 312 422"
+                backgroundColor="#f3f3f3"
+                foregroundColor="#ecebeb">
+                <rect x="0" y="0" rx="6" ry="6" width="312" height="312" />
+                <rect x="0" y="333" rx="6" ry="6" width="267" height="20" />
+                <rect x="0" y="395" rx="6" ry="6" width="75" height="24" />
+                <rect x="265" y="375" rx="6" ry="6" width="44" height="45" />
+              </ContentLoader>
+            ) : (
+              <Card {...obj} />
+            )}
           </li>
         ))}
       </ul>
