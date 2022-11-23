5fc5fa06aef887f8a80eb4daf290aa7295a66a16
Some1 <111959143+s1i1@users.noreply.github.com>
2022-11-23 14:11:19 +0300

    В product-card прикрутил скелетон

--- src/features/product-card/ui/index.tsx
+++ src/features/product-card/ui/index.tsx
@@ -3,6 +3,8 @@ import cn from 'classnames';
 import { Card } from 'shared/ui';
 import { Heading } from 'entities/heading';
 import { useGetItemsQuery } from 'shared/api';
+import ContentLoader from 'react-content-loader';
+
 import styles from './styles.module.scss';
 
 type ProductCardProps = {
@@ -13,7 +15,7 @@ type ProductCardProps = {
 const ProductCard: React.FC<ProductCardProps> = ({ headerTitle, searchTag }) => {
   const [pageNum, setPageNum] = React.useState(1);
 
-  const { data: FeaturedItems } = useGetItemsQuery({
+  const { data: FeaturedItems, isFetching } = useGetItemsQuery({
     limit: 4,
     pageNumber: pageNum,
     tag: searchTag,
@@ -25,7 +27,22 @@ const ProductCard: React.FC<ProductCardProps> = ({ headerTitle, searchTag }) => 
       <ul className={styles.list}>
         {FeaturedItems?.map((obj, index) => (
           <li key={index}>
-            <Card {...obj} searchTag={searchTag} />
+            {isFetching ? (
+              <ContentLoader
+                speed={1}
+                width={312}
+                height={422}
+                viewBox="0 0 312 422"
+                backgroundColor="#f3f3f3"
+                foregroundColor="#ecebeb">
+                <rect x="0" y="0" rx="6" ry="6" width="312" height="312" />
+                <rect x="0" y="333" rx="6" ry="6" width="267" height="20" />
+                <rect x="0" y="395" rx="6" ry="6" width="75" height="24" />
+                <rect x="265" y="375" rx="6" ry="6" width="44" height="45" />
+              </ContentLoader>
+            ) : (
+              <Card {...obj} searchTag={searchTag} />
+            )}
           </li>
         ))}
       </ul>
